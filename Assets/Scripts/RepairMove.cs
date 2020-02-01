9f8d67f36d6dbfd5878a199899dcc1c49c685332
Ben Richards <ben.richards@ioof.com.au>
2020-02-01 15:26:13 +1100

    Repair move

---
+++ Assets/Scripts/RepairMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairMove : MonoBehaviour
+{
+    public GameObject body;
+
+    public DamageHandler damageHandler;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }   
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Y) && damageHandler.damage > 0) {
+            damageHandler.damage = damageHandler.damage - 2;
+        }
+    }
+}
