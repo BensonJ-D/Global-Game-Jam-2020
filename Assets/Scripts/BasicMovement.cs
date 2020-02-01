1280c377f9a0608c58462407fb5a60d6f0d6272e
Molly Steer <mollysteer@Mollys-MacBook-Pro.local>
2020-02-01 16:08:52 +1100

    fixing problems

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -18,24 +18,16 @@ public class BasicMovement : MonoBehaviour
 
         transform.position = new Vector2(transform.position.x + (0.1f * Input.GetAxis("Axis 11")), transform.position.y);
 
-        if (Input.GetKey(KeyCode.A))
-        {
-<<<<<<< HEAD
-            transform.position = new Vector2(transform.position.x + -0.03f, transform.position.y);
-=======
-            transform.position = new Vector2(transform.position.x + -0.1f, transform.position.y);
->>>>>>> 9f8d67f36d6dbfd5878a199899dcc1c49c685332
-            //transform.GetComponentInChildren<Rigidbody2D>(). (new Vector2(transform.position.x + -0.1f, transform.position.y * 10f), ForceMode2D.Force);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-<<<<<<< HEAD
-            transform.position = new Vector2(transform.position.x + 0.03f, transform.position.y);
-=======
-            transform.position = new Vector2(transform.position.x + 0.1f, transform.position.y);
->>>>>>> 9f8d67f36d6dbfd5878a199899dcc1c49c685332
-            //transform.GetComponentInChildren<Rigidbody2D>().AddRelativeForce(Vector2.right * 5f);
-        }
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.position = new Vector2(transform.position.x + -0.03f, transform.position.y);
+
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.position = new Vector2(transform.position.x + 0.1f, transform.position.y);
+        //    //transform.GetComponentInChildren<Rigidbody2D>().AddRelativeForce(Vector2.right * 5f);
+        //}
         if (Input.GetKeyDown(KeyCode.Space) && jumps>0)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 60, ForceMode2D.Impulse);
