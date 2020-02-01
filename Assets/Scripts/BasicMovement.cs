4b8a636347af3b0e842bc829c1c318a236b780ec
Ben Richards <ben.richards@ioof.com.au>
2020-02-01 14:37:26 +1100

    Damage!

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -16,12 +16,12 @@ public class BasicMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector2(transform.position.x + -0.01f, transform.position.y);
+            transform.position = new Vector2(transform.position.x + -0.1f, transform.position.y);
             //transform.GetComponentInChildren<Rigidbody2D>(). (new Vector2(transform.position.x + -0.1f, transform.position.y * 10f), ForceMode2D.Force);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position = new Vector2(transform.position.x + 0.01f, transform.position.y);
+            transform.position = new Vector2(transform.position.x + 0.1f, transform.position.y);
             //transform.GetComponentInChildren<Rigidbody2D>().AddRelativeForce(Vector2.right * 5f);
         }
         if (Input.GetKeyDown(KeyCode.Space))
---
+++ Assets/Scripts/DamageHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHandler : MonoBehaviour
+{
+    public Vector2 oldVelocity;
+    public GameObject body;
+
+    public float damage = 0;    
+    private float fudgeFactor = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        Vector2 newVelocity = body.GetComponent<Rigidbody2D>().velocity;
+        oldVelocity.Set(newVelocity.x, newVelocity.y);
+    }
+
+    /// <summary>
+    /// Sent when an incoming collider makes contact with this object's
+    /// collider (2D physics only).
+    /// </summary>
+    /// <param name="other">The Collision2D data associated with this collision.</param>
+    void OnCollisionEnter2D(Collision2D other)
+{
+        string otherHit = other.gameObject.tag;
+        if(otherHit!=body.tag && otherHit != "ground") {
+            Vector2 impulse = (body.GetComponent<Rigidbody2D>().velocity - oldVelocity)*body.GetComponent<Rigidbody2D>().mass;
+            damage = damage+impulse.magnitude/body.GetComponent<Rigidbody2D>().mass*fudgeFactor;
+
+            body.GetComponent<Rigidbody2D>().AddForce(impulse.normalized*damage);
+        }
+    }
+}
