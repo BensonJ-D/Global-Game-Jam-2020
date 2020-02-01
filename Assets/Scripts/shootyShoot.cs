3de1c5a1d7c9c4807e5cbcef889e5ee1343d7675
mitchpatto <mitchell.patterson1@gmail.com>
2020-02-01 16:15:22 +1100

    Discount Shooting Mechanic
    Unsure how helpful, use the right arrow to shoot

---
+++ Assets/Scripts/shootyShoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootyShoot : MonoBehaviour
+{
+
+    public GameObject booleet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("right"))
+        {
+           GameObject b = Instantiate(booleet, gameObject.transform.position, transform.rotation);
+            b.GetComponent<Rigidbody2D>().velocity = -transform.right * 10;
+        }
+    }
+}
