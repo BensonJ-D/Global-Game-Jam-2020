bcd56140d4b194b53c1e6bc195a77217fd9497dc
sage3648 <sage.stainsbys@gmail.com>
2020-02-01 13:53:58 +1100

    Mace to the face

---
+++ Assets/Scripts/sceneManagement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManagement : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Loading Scene");
+    }
+
+    public void Load(int sceneNumber)
+    {
+        Debug.Log("sceneBuildIndex to load: " + sceneNumber);
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
