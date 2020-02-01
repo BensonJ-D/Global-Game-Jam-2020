ca7fedcf188b53affbd20c559b9931403f8f03d0
Molly Steer <mollysteer@Mollys-MacBook-Pro.local>
2020-02-01 15:55:53 +1100

    Added activate arm, driven by arm movement

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
+    bool onGround = false;
+    int jumps = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +18,30 @@ public class BasicMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector2(transform.position.x + -0.01f, transform.position.y);
+            transform.position = new Vector2(transform.position.x + -0.03f, transform.position.y);
             //transform.GetComponentInChildren<Rigidbody2D>(). (new Vector2(transform.position.x + -0.1f, transform.position.y * 10f), ForceMode2D.Force);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position = new Vector2(transform.position.x + 0.01f, transform.position.y);
+            transform.position = new Vector2(transform.position.x + 0.03f, transform.position.y);
             //transform.GetComponentInChildren<Rigidbody2D>().AddRelativeForce(Vector2.right * 5f);
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && jumps>0)
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 8, ForceMode2D.Impulse);
-
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 60, ForceMode2D.Impulse);
+            jumps--;
         }
 
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Platform")
+        {
+            onGround = true;
+            jumps = 2;
+        }
+       
     }
 }
---
+++ Assets/Scripts/activateArm.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateArm : MonoBehaviour
+{
+    public float force = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            //Destroy(gameObject);
+            gameObject.GetComponentInChildren<Rigidbody2D>().AddForce(Vector2.left * force, ForceMode2D.Impulse);
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Destroy(gameObject);
+            gameObject.GetComponentInChildren<Rigidbody2D>().AddForce(Vector2.right * force, ForceMode2D.Impulse);
+
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //Destroy(gameObject);
+            gameObject.GetComponentInChildren<Rigidbody2D>().AddForce(Vector2.up * force, ForceMode2D.Impulse);
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //Destroy(gameObject);
+            gameObject.GetComponentInChildren<Rigidbody2D>().AddForce(Vector2.down * force, ForceMode2D.Impulse);
+
+        }
+
+
+    }
+}
