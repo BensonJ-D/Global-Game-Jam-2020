7526e3c0a914b386971d8da399bd3d8918b56460
sage3648 <sage.stainsbys@gmail.com>
2020-02-02 13:26:29 +1100

    CFS-80085

---
+++ Assets/Scripts/RandomMusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMusicPlayer : MonoBehaviour
+{
+    public AudioClip[] Songs;
+    int songChoice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        songChoice = Random.Range(0, Songs.Length);
+        gameObject.GetComponent<AudioSource>().clip = Songs[songChoice];
+        gameObject.GetComponent<AudioSource>().Play();
+    }
+
+}
---
+++ Assets/Scripts/menuCustomiseOverseer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuCustomiseOverseer : MonoBehaviour
+{
+
+    // 1_playerOneSkin, 2_PlayerTwoSkin,
+    public Color[] skinData = new Color[2];
+
+    // 1_PlayerOneRW, 2_PlayerOneLW, 3_PlayerTwoRW, 4_PlayerTwoLW;
+    public weapon[] weaponData = new weapon[4];
+
+    public weapon[] weaponList;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void recieveSkins(Color skin, int index)
+    {
+        Debug.Log("SETTING: "+ skin + "   " +"["+index+"]");
+        skinData[index-1] = skin;
+    }
+
+    public void recieveWeapons(weapon weapon, int index)
+    {
+        Debug.Log("SETTING: " + weapon + "   " + "[" + index + "]");
+        weaponData[index - 3] = weapon;
+    }
+
+}
+
+[System.Serializable]
+public class weapon
+{
+    public bool disabled;
+    public string name;
+    public Sprite sprite;
+    public GameObject prefab;
+}
